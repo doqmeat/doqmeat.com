9390f74a5d5c5939122aa3bc8781226b0815f2bc
des <desteny282@gmail.com>
2024-06-11 23:42:22 -0400

    minor fixes/tweaks

--- doqmeat/microblog/picture.js
+++ doqmeat/microblog/picture.js
@@ -1,11 +1,12 @@
-		function showPic(img, text) {
-			var i;
-			var pic = document.getElementsByClassName('picture');
+		function showPic(img, text, caption) {
+			var pic = document.getElementsByClassName("picture");
 			var picDiv = document.getElementById("picture-win");
-			var picTitle = document.getElementsByClassName('picture-url');
+			var picTitle = document.getElementsByClassName("picture-title");
+			var picCaption = document.getElementsByClassName("picture-caption");
 			pic[0].src = img;
 			pic[0].alt = text;
 			picTitle[0].innerText = img;
+			picCaption[0].innerText = caption;
 			picDiv.style.display = "block";
 		}
 
