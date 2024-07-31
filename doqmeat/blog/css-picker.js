a752024faaa3ddad4a6a9563e26323f369fe18df
Desteny <desteny282@gmail.com>
2024-07-31 14:18:49 -0400

    figured out localstorageeee for js

--- doqmeat/blog/css-picker.js
+++ doqmeat/blog/css-picker.js
@@ -1,12 +1,23 @@
 //code taken from this site! https://www.geeksforgeeks.org/how-to-switch-between-multiple-css-stylesheets-using-javascript//
 
 function toggleTheme() {
+	var sheet = document.getElementsByClassName("theme")[0];
+	if (sheet.getAttribute("href") == "/blog/light.css") {
+		sheet.setAttribute("href", "/blog/dark.css");
+		localStorage.setItem("theme", "/blog/dark.css");
+	} else {
+		sheet.setAttribute("href", "/blog/light.css");
+		localStorage.setItem("theme", "/blog/light.css");
+	}
+}
 
-			var sheet = document.getElementsByClassName('theme')[0];
+window.onload = checkTheme();
 
-			if (sheet.getAttribute('href') == '/blog/light.css') {
-				sheet.setAttribute('href', '/blog/dark.css');
-			} else {
-				sheet.setAttribute('href', '/blog/light.css');
-			}
-		}
+function checkTheme() {
+	var sheet = document.getElementsByClassName("theme")[0];
+	var localTheme = localStorage.getItem("theme");
+	sheet.setAttribute("href", localTheme);
+	if (localTheme == null) {
+		sheet.setAttribute("href", "/blog/light.css");
+	}
+}
