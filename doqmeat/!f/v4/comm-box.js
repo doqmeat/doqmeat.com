bb62e87f35bbfa76b509745de16683c697e3be95
Desteny <desteny282@gmail.com>
2024-10-20 23:24:52 -0400

    minor changes to index

--- doqmeat/!f/v4/comm-box.js
+++ doqmeat/!f/v4/comm-box.js
@@ -1,16 +1,15 @@
 function toggleComm(box) {
-	var clickBox = document.getElementById(box);
-	var others = document.getElementsByClassName("comm-box");
-	var i = 0;
-	for (i; i < others.length; i++) {
-		others[i].style.display = "none";
-	}
-	clickBox.style.display = "block";
+  var clickBox = document.getElementById(box);
+  var others = document.getElementsByClassName("comm-box");
+  var i = 0;
+  for (i; i < others.length; i++) {
+    others[i].style.display = "none";
+  }
+  clickBox.style.display = "block";
 }
 
 function defaultOpen() {
-	toggleComm('listing');
-	var button = document.getElementById("default-button");
+  toggleComm("listing");
 }
 
 window.onload = defaultOpen();
