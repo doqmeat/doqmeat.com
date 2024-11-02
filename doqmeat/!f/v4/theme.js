3cef606bd064f35191959769776679725856f875
Desteny <desteny282@gmail.com>
2024-11-02 11:48:45 -0400

    minor changes to homepage

--- doqmeat/!f/v4/theme.js
+++ doqmeat/!f/v4/theme.js
@@ -1,16 +1,16 @@
-		window.onload = checkTheme();
+window.onload = checkTheme();
 
-		function pickTheme(value) {
-			var sheet = document.getElementsByClassName('theme')[0];
-			sheet.href = value;
-			localStorage.setItem("theme", value);
-		}
+function pickTheme(value) {
+  var sheet = document.getElementsByClassName("theme")[0];
+  sheet.href = value;
+  localStorage.setItem("theme", value);
+}
 
-		function checkTheme() {
-			var sheet = document.getElementsByClassName('theme')[0];
-			var localTheme = localStorage.getItem("theme");
-			sheet.href = localTheme;
-			if (localTheme == null) {
-				sheet.href = "!f/v4/sunset.css";
-			}
-		}
+function checkTheme() {
+  var sheet = document.getElementsByClassName("theme")[0];
+  var localTheme = localStorage.getItem("theme");
+  sheet.href = localTheme;
+  if (localTheme == null) {
+    sheet.href = "!f/v4/sunset/style.css";
+  }
+}
