7eed43518c94053c2e48ce16bf86bdb98e3997be
doqmeat <desteny282@gmail.com>
2025-03-02 15:03:35 -0400

    v5 homepage files

---
+++ doqmeat/!f/v5/last-fm.js
@@ -0,0 +1,44 @@
+//    code from here: https://github.com/biancarosa/lastfm-last-played
+
+let user = "lakiitu";
+let url =
+  "https://lastfm-last-played.biancarosa.com.br/" + user + "/latest-song";
+let song = document.querySelector("#song");
+let album = document.querySelector("#album");
+let songL = document.querySelector("#song-link");
+let listeningP = document.querySelector("#listening");
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    let albumCover = json["track"]["image"][2]["#text"];
+    let trackName = json["track"]["name"];
+    let albumName = json["track"]["album"]["#text"];
+    let artist = json["track"]["artist"]["#text"];
+    let trackLink = json["track"]["url"];
+
+    // insert thingies into HTML
+
+    songL.href = trackLink;
+
+    songL.title = "listen to " + trackName + " by " + artist + " on last.fm";
+
+    // in case there's no album info
+    if (albumCover == "") {
+      album.innerHTML = "<span>album cover not available<br>(´•︵•`)</span>";
+      albumName = "N/A";
+    } else {
+      album.innerHTML = '<img src="' + albumCover + '">';
+    }
+
+    song.innerHTML =
+      '<span class="icons">🎵</span> ' +
+      trackName +
+      "<br>" +
+      '<span class="icons">👤</span> ' +
+      artist +
+      '<br> <span class="icons">🖼️</span> ' +
+      albumName;
+  });
---
+++ doqmeat/!f/v5/script.js
@@ -0,0 +1,34 @@
+// changes position and width depending on the ratio of the img inside of it :p
+function piclogWidth() {
+  let piclog = document.getElementById("piclog");
+  let image = document.querySelector("#piclog img");
+  let tape = document.querySelector("#tape");
+  if (image.naturalWidth > image.naturalHeight) {
+    tape.style.left = "30%";
+    piclog.style.width = "300px";
+    piclog.style.height = "295px";
+  } else {
+    piclog.style.width = "250px";
+    piclog.style.height = "360px";
+  }
+}
+
+// the different sections of the notepad
+function showBox(boxID) {
+  let box = document.getElementById(boxID);
+  let allBoxes = document.getElementsByClassName("notepad-boxes");
+  let tabs = document.querySelectorAll("#tabs button");
+  let i;
+
+  for (i = 0; i < allBoxes.length; i++) {
+    allBoxes[i].style.display = "none";
+  }
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("active");
+  }
+  box.style.display = "block";
+  event.currentTarget.className += " active";
+}
+
+piclogWidth();
+document.getElementById("default").click();
