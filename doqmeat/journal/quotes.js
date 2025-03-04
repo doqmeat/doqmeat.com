498ca5d1d671a4167ff093abf27fa3343501c6dd
doqmeat <desteny282@gmail.com>
2025-03-03 22:50:15 -0400

    journal stuff

---
+++ doqmeat/journal/quotes.js
@@ -0,0 +1,78 @@
+const quotes = [
+  {
+    text: "a chocolate sepia record that is important to me and no one else",
+    source: "999 - siinamota",
+    link: "https://vocaloidlyrics.fandom.com/wiki/999/siinamota",
+  },
+
+  {
+    text: "in this universe its not a coincidence that i’ve met myself. oh, i know now why the waving flowers bloom",
+    source: "one & only",
+    author: "loona",
+    link: "",
+  },
+
+  {
+    text: "to me, you will be unique in all the world. and to you i shall be unique in all the world",
+    source: "the little prince",
+    author: "antoine de saint-exupery",
+    link: "",
+  },
+
+  {
+    text: "“I don’t mind where we go. To me, anywhere is fine.” But this place is beautiful. Scary, but beautiful. To think that no one’s been here but us-",
+    source: "halo",
+    author: "siinamota",
+    link: "",
+  },
+
+  {
+    text: "when i choose to see the good side of things, im not being naive. it is strategic and necessary. it’s how ive learned to survive through everything.",
+    source: "everything everywhere all at once",
+    author: "dir. the daniels",
+    link: "",
+  },
+
+  {
+    text: "It's hard to believe that it's over, isn't it? Funny how we get attached to the struggle.",
+    source: "celeste",
+    author: "-",
+    link: "",
+  },
+  {
+    text: "i'm sure that in time, in time, i'll forget all about myself and advance on. it's just, i... it doesn't seem like i'll be able to forget about you.",
+    source: "there is someone i can’t meet, even in a dream",
+    author: "siinamota",
+    link: "",
+  },
+
+  {
+    text: "the only way to not feel pain is to never feel love. and thats not a life worth living.",
+    source: "therapist guys omg",
+    author: "-",
+    link: "",
+  },
+
+  {
+    text: "i dont think this is trash. if you’re going to throw it away, i’ll take it. i made the decision to consider my feelings more. and you need to pick up things you feel are important",
+    source: "mob psycho 100",
+    author: "one",
+    link: "",
+  },
+
+  {
+    text: "Let’s move straight ahead, no matter where we end up. Swaying, dozing off, no matter where we end up, we won’t get to where we want to be.",
+    source: "halo - siinamota",
+    link: "",
+  },
+
+  {
+    text: "i thought i was going after my dreams. and yet im scared, im scared, im scared",
+    source: "young girl a - siinamota",
+    link: "",
+  },
+];
+
+let random = Math.floor(Math.random() * quotes.length);
+
+console.log(Math.random());
