0b38f5b20ab33cb66614d08c38b3e28415de3f57
doqmeat <desteny282@gmail.com>
2025-03-08 19:00:52 -0400

    microblog post

--- doqmeat/microblog/picture.js
+++ doqmeat/microblog/picture.js
@@ -1,22 +1,23 @@
-		function showPic(img, text, caption) {
-			var pic = document.getElementById("picture");
-			var picDiv = document.getElementById("picture-win");
-			var picTitle = document.getElementById("picture-title");
-			var picCaption = document.getElementById("picture-caption");
-			var link = document.getElementById("pic-link");
-			link.href = img;
-			pic.src = img;
-			pic.alt = text;
-			picTitle.innerText = img;
-			picCaption.innerText = caption;
-			picDiv.style.display = "block";
-		}
+let pic = document.getElementById("picture");
+let picDiv = document.getElementById("picture-win");
+let picTitle = document.getElementById("picture-title");
+let picCaption = document.getElementById("picture-caption");
+let link = document.getElementById("pic-link");
 
-		function toggleclick(window) {
-			var popup = document.getElementById(window);
-			if (popup.style.display === "block") {
-				popup.style.display = "none";
-			} else {
-				popup.style.display = "block";
-			}
-		}
+function showPic(img, text, caption) {
+	link.href = img;
+	pic.src = img;
+	pic.alt = text;
+	picTitle.innerText = img;
+	picCaption.innerText = caption;
+	picDiv.style.display = "block";
+}
+
+function toggleclick(window) {
+	let popup = document.getElementById(window);
+	if (popup.style.display === "block") {
+		popup.style.display = "none";
+	} else {
+		popup.style.display = "block";
+	}
+}
