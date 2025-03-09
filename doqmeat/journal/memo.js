9b24ba23b72bf58a67e61dac1a1d7e79b50a0129
doqmeat <desteny282@gmail.com>
2025-03-09 14:51:15 -0400

    added some of my fav quotes in a JS array and one of them pops up on refresh

--- doqmeat/journal/memo.js
+++ doqmeat/journal/memo.js
@@ -3,19 +3,28 @@ let button = document.querySelector("nav button");
 let winSize = window.matchMedia("(max-width:1300px)");
 
 function closeMemos() {
-  for (i = 0; i < memos.length; i++) {
-    let memo = memos[i];
-    if (memo.style.display == "none") {
-      memo.style.display = "block";
-      button.innerHTML = "hide memos";
-    } else {
-      memo.style.display = "none";
-      button.innerHTML = "show memos";
-    }
-  }
+	for (i = 0; i < memos.length; i++) {
+		let memo = memos[i];
+		if (memo.style.display == "none") {
+			memo.style.display = "block";
+			button.innerHTML = "hide memos";
+		} else {
+			memo.style.display = "none";
+			button.innerHTML = "show memos";
+		}
+	}
+}
+
+for (i = 0; i < memos.length; i++) {
+	let memoClick = memos[i];
+	memoClick.onclick = function (posindex) {
+		let memoID = document.getElementById(this.id);
+		memoID.style.zIndex = i++;
+		console.log(memoID.style.zIndex);
+	};
 }
 
 // if screen is small enough, hide memos by default
 if (winSize.matches) {
-  closeMemos();
+	closeMemos();
 }
