a755a022b9ed7d5d73c2e8cd40089e7d1997c34e
doqmeat <desteny282@gmail.com>
2025-03-16 01:14:42 -0400

    changes to files

--- doqmeat/!f/v5/last-fm.js
+++ doqmeat/!f/v5/last-fm.js
@@ -1,8 +1,6 @@
 //    code from here: https://github.com/biancarosa/lastfm-last-played
 
-let user = "lakiitu";
-let url =
-  "https://lastfm-last-played.biancarosa.com.br/" + user + "/latest-song";
+let url = "https://lastfm-last-played.biancarosa.com.br/lakiitu/latest-song";
 let song = document.querySelector("#song");
 let album = document.querySelector("#album");
 let songL = document.querySelector("#song-link");
@@ -11,36 +9,36 @@ let listeningP = document.querySelector("#listening");
 // https://maxpixels.moe/resources/lastfm-widget/
 
 fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (json) {
-    let albumCover = json["track"]["image"][2]["#text"];
-    let trackName = json["track"]["name"];
-    let albumName = json["track"]["album"]["#text"];
-    let artist = json["track"]["artist"]["#text"];
-    let trackLink = json["track"]["url"];
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (json) {
+		let albumCover = json["track"]["image"][2]["#text"];
+		let trackName = json["track"]["name"];
+		let albumName = json["track"]["album"]["#text"];
+		let artist = json["track"]["artist"]["#text"];
+		let trackLink = json["track"]["url"];
 
-    // insert thingies into HTML
+		// insert thingies into HTML
 
-    songL.href = trackLink;
+		songL.href = trackLink;
 
-    songL.title = "listen to " + trackName + " by " + artist + " on last.fm";
+		songL.title = "listen to " + trackName + " by " + artist + " on last.fm";
 
-    // in case there's no album info
-    if (!albumName) {
-      album.innerHTML = "<span>album cover not available<br>(´•︵•`)</span>";
-      albumName = "N/A";
-    } else {
-      album.innerHTML = '<img src="' + albumCover + '">';
-    }
+		// in case there's no album info
+		if (!albumName) {
+			album.innerHTML = "<span>album cover not available<br>(´•︵•`)</span>";
+			albumName = "N/A";
+		} else {
+			album.innerHTML = '<img src="' + albumCover + '">';
+		}
 
-    song.innerHTML =
-      '<span class="icons">🎵</span> ' +
-      trackName +
-      "<br>" +
-      '<span class="icons">👤</span> ' +
-      artist +
-      '<br> <span class="icons">🖼️</span> ' +
-      albumName;
-  });
+		song.innerHTML =
+			'<span class="icons">🎵</span> ' +
+			trackName +
+			"<br>" +
+			'<span class="icons">👤</span> ' +
+			artist +
+			'<br> <span class="icons">🖼️</span> ' +
+			albumName;
+	});
