c7a2f72f95f068e323c8e38a546566b08b510ff9
doqmeat <desteny282@gmail.com>
2025-04-06 12:47:53 -0400

    added april page to scrapbook + a new script that replaces text on the HTML file with characters

---
+++ doqmeat/scrapbook/chars.js
@@ -0,0 +1,14 @@
+// selects element. i want to target the whole page so we going with body
+let pageBody = document.querySelector("body").innerHTML;
+
+// replace text using .replace
+let replacement = pageBody // /g means it will change each pattern it finds instead of only one instance
+	.replace(/::repeat::/g, "⟳")
+	.replace(/::finished::/g, "✔")
+	.replace(/::ongoing::/g, "□")
+	.replace(/::dropped::/g, "✘")
+	.replace(/::two stars::/g, "★★☆☆☆")
+	.replace(/::three stars::/g, "★★★☆☆")
+	.replace(/::four stars::/g, "★★★★☆")
+	.replace(/::five stars::/g, "★★★★★");
+document.querySelector("body").innerHTML = replacement;
