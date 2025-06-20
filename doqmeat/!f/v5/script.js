f6a18f407f6af3dba47e573725723ab355d33015
doqmeat <desteny282@gmail.com>
2025-06-20 17:52:58 -0400

    added a script to homepage that changes the background color depending on the month

--- doqmeat/!f/v5/script.js
+++ doqmeat/!f/v5/script.js
@@ -1,3 +1,6 @@
+piclogWidth();
+changeBackgroundColor();
+
 // changes position and width of the element depending on the ratio of the img inside of it :p
 function piclogWidth() {
 	let piclog = document.getElementById("piclog");
@@ -19,10 +22,7 @@ function piclogWidth() {
 	}
 }
 
-// checks for piclog width on page load
-piclogWidth();
-
-// the different sections of the notepad
+// use: displays the different sections of the notepad
 function showBox(boxID) {
 	let box = document.getElementById(boxID);
 	let allBoxes = document.getElementsByClassName("notepad-boxes");
@@ -38,11 +38,10 @@ function showBox(boxID) {
 	box.style.display = "block";
 	event.currentTarget.className += " active";
 }
-
-// clicks the default tab on the notepad
+// clicks the default tab on the notepad by default on reload
 document.getElementById("default").click();
 
-// when clicking the 'chatbox' tab, this popup will appear asking you if you agree with the rules
+// use: when clicking the 'chatbox' tab, this popup will appear asking you if you agree with the rules
 function chatPopup(choice = "none") {
 	let agree = localStorage.getItem("messageAgree"); // so it doesn't ask you every time
 	let chatbox = document.getElementById("chatbox-frame");
@@ -72,5 +71,51 @@ function chatPopup(choice = "none") {
 	} else {
 		// the popup won't appear and you'll get access to the chat
 		chatbox.style.display = "block";
+	}
+}
+
+// use: change body background color depending on the month!
+function changeBackgroundColor() {
+	let body = document.querySelector("body"); // target homepage body
+	const myDate = new Date(); // create date object
+
+	// switch background color depending on the month
+	switch (myDate.getMonth()) {
+		case 0: // jan
+			body.style.backgroundColor = "rgb(229, 237, 229)";
+			break;
+		case 1: // february
+			body.style.backgroundColor = "#ffeaf6";
+			break;
+		case 2: // march
+			body.style.backgroundColor = "rgb(236, 245, 211)";
+			break;
+		case 3: // april
+			body.style.backgroundColor = "rgb(228, 224, 251)";
+			break;
+		case 4: // may
+			body.style.backgroundColor = "rgb(232, 243, 232)";
+			break;
+		case 5: // june
+			body.style.backgroundColor = "#FFF5D3";
+			break;
+		case 6: // july
+			body.style.backgroundColor = "#f9f0e6";
+			break;
+		case 7: // august
+			body.style.backgroundColor = "rgb(249, 230, 230)";
+			break;
+		case 8: // sep
+			body.style.backgroundColor = "rgb(226, 244, 244)";
+			break;
+		case 9: // oct
+			body.style.backgroundColor = "rgb(251, 235, 205)";
+			break;
+		case 10: //nov
+			body.style.backgroundColor = "rgb(253, 234, 250)";
+			break;
+		case 11: // dec
+			body.style.backgroundColor = "#ebf2fb";
+			break;
 	}
 }
