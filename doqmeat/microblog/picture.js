a2f271e5a8e850b330c584290b45377e3b95791c
doqmeat <desteny282@gmail.com>
2025-07-23 15:14:57 -0400

    added notes to microblog

--- doqmeat/microblog/picture.js
+++ doqmeat/microblog/picture.js
@@ -14,10 +14,31 @@ function showPic(img, text, caption) {
 }
 
 function toggleclick(window) {
+	let folderIcon = document.querySelector("#folder img");
 	let popup = document.getElementById(window);
 	if (popup.style.display === "block") {
 		popup.style.display = "none";
+		if (window == "notes") {
+			folderIcon.src = "folder-closed.png";
+		}
 	} else {
 		popup.style.display = "block";
+		if (window == "notes") {
+			folderIcon.src = "folder-opened.png";
+		}
 	}
+}
+
+function toggleNote(noteName) {
+	let fileName = document.querySelector("#notepad small");
+	fileName.innerText = noteName + ".txt";
+	let notepad = document.getElementById("notepad");
+	notepad.style.display = "block"; // opens notepad div
+	let selectedNote = document.getElementById(noteName);
+	let notes = document.getElementsByClassName("notes");
+	for (let i = 0; i < notes.length; i++) {
+		// hides all other notes
+		notes[i].style.display = "none";
+	}
+	selectedNote.style.display = "block"; // shows selected note
 }
