c19503bfd3a3f729688cec6ecfaf99c38c6eb2a0
doqmeat <desteny282@gmail.com>
2025-07-27 04:57:23 -0400

    some refactoring in the script

--- doqmeat/art/script.js
+++ doqmeat/art/script.js
@@ -5,50 +5,59 @@ let picture = document.querySelector("#pop-up #pic");
 let container = document.querySelector("body");
 let captions = document.getElementsByClassName("captions");
 let buttons = document.querySelectorAll("#sidebar button");
+let i;
+
+// adds a listener for when a button is clicked that adds the active class. yay
+for (i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener("click", function () {
+		this.classList.add("active");
+	});
+}
+
+for (i = 0; i < boxes.length; i++) {
+	boxes[i].addEventListener("click", function () {
+		// get img link
+		picture.src = this.src;
+		picture.alt = this.alt;
+		// captions
+		altCaption.innerText = this.alt;
+		container.style.overflow = "hidden";
+		popUp.style.display = "block";
+		// img width
+		if (this.naturalWidth > this.naturalHeight) {
+			picture.style.width = "70%";
+		} else {
+			picture.style.width = "50%";
+		}
+		if (!this.id) {
+			// checks if img has an id.
+			return;
+		} else {
+			// if it has an id...
+			let postCap = document.getElementById(this.id + "-caption");
+			altCaption.innerHTML = postCap.innerHTML; // injects the div under the pic
+		}
+	});
+}
 
 function showCat(className) {
-  let classBox = document.getElementsByClassName(className);
-  for (i = 0; i < buttons.length; i++) {
-    buttons[i].classList.remove("active");
-  }
-  event.currentTarget.className += " active";
-  for (i = 0; i < boxes.length; i++) {
-    boxes[i].style.display = "none";
-  }
-  for (i = 0; i < classBox.length; i++) {
-    classBox[i].style.display = "inline-block";
-  }
+	let classBox = document.getElementsByClassName(className);
+	for (i = 0; i < buttons.length; i++) {
+		// removes any prior active button
+		buttons[i].classList.remove("active");
+	}
+
+	for (i = 0; i < boxes.length; i++) {
+		boxes[i].style.display = "none";
+	}
+	for (i = 0; i < classBox.length; i++) {
+		classBox[i].style.display = "inline-block";
+	}
 }
 
 document.getElementById("default").click();
 
-for (i = 0; i < boxes.length; i++) {
-  let imgClick = boxes[i];
-  imgClick.onclick = function (windowpopup) {
-    // get img link
-    picture.src = this.src;
-    picture.alt = this.alt;
-    // captions
-    altCaption.innerText = this.alt;
-    container.style.overflow = "hidden";
-    popUp.style.display = "block";
-    // img width
-    if (this.naturalWidth > this.naturalHeight) {
-      picture.style.width = "70%";
-    } else {
-      picture.style.width = "50%";
-    }
-    if (this.id == "") {
-      return;
-    } else {
-      let postCap = document.getElementById(this.id + "-caption");
-      altCaption.innerHTML = postCap.innerHTML;
-    }
-    console.log(picture.style.width);
-  };
-}
-
 function closePopUp() {
-  container.style.overflow = "auto";
-  popUp.style.display = "none";
+	container.style.overflow = "auto";
+	popUp.style.display = "none";
 }
